d8f1d45cf9e08d9492bc926b673fda7f7c016c15
Tushar Anekar <anekar.tushar@gmail.com>
2024-08-16 14:42:14 +0530

    fix- change api calls to https

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const getAllMoviesService = async () =>
-  await axios("http://www.omdbapi.com/?apikey=b9cb92f7&s=war&type=movie");
+  await axios("https://www.omdbapi.com/?apikey=b9cb92f7&s=war&type=movie");
 
 const getMovieDetailsService = async (imdbID) =>
-  await axios(`http://www.omdbapi.com/?apikey=b9cb92f7&i=${imdbID}`);
+  await axios(`https://www.omdbapi.com/?apikey=b9cb92f7&i=${imdbID}`);
 
 const getMoviePosterByIdService = async (imdbID) =>
-  await axios(`http://img.omdbapi.com/?apikey=b9cb92f7&i=${imdbID}`);
+  await axios(`https://img.omdbapi.com/?apikey=b9cb92f7&i=${imdbID}`);
 
 export {
   getAllMoviesService,
