372e94a4121de6624cfe6af51017977cf6f6f658
Tushar Anekar <anekar.tushar@gmail.com>
2024-08-16 16:49:18 +0530

    feat- navigate to home on logo click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import PlayCircleFilledWhiteOutlinedIcon from "@mui/icons-material/PlayCircleFilledWhiteOutlined";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  function handleHomeLogo() {
+    navigate("/");
+  }
+
   return (
     <header className="flex justify-between py-4 text-gray-300">
-      <PlayCircleFilledWhiteOutlinedIcon />
+      <button onClick={handleHomeLogo}>
+        <PlayCircleFilledWhiteOutlinedIcon />
+      </button>
 
       <div className="flex gap-4">
         <NavLink to={"/"}>Movies</NavLink>
